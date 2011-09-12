452ce097d5ee4ae54e149f5470af81369693241b
Jonathan Fokkan <jonathan.fokkan@mail.mcgill.ca>
2011-09-12 13:22:10 -0400

    ADDED MultpyConstant Method

--- COMP250/Polynomial.java
+++ COMP250/Polynomial.java
@@ -1,3 +1,8 @@
-public class Polynomial extends poly {
+public class Polynomial extends Poly {
 
+    public void MultpyConstant(double value) {
+        for(int i = 0; i < coefficients.length; i++) {
+            coefficients[i] *= value;
+        }
+    }
 }
