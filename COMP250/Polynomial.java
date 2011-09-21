a9903b74729bdba3b8655f740dab8d5605950070
Jonathan Fokkan <jonathan.fokkan@mail.mcgill.ca>
2011-09-21 00:22:12 -0400

    TO DO last question

--- COMP250/Polynomial.java
+++
@@ -1,8 +0,0 @@
-public class Polynomial extends Poly {
-
-    public void MultpyConstant(double value) {
-        for(int i = 0; i < coefficients.length; i++) {
-            coefficients[i] *= value;
-        }
-    }
-}
