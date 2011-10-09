b36e9a32415c2bf8f9f53eec210c8f3fd03394b2
Jonathan Fokkan <jonathan.fokkan@mail.mcgill.ca>
2011-10-09 11:25:23 -0400

    DONE up to Q3

--- COMP250/2_Assignment/LongestSubsequence.java
+++ COMP250/2_Assignment/LongestSubsequence.java
@@ -1,12 +1,37 @@
+import java.util.Arrays;
+
 public class LongestSubsequence {
 
     public static int LongestSubsequence (int[] a) {
 	// your code goes here
 	// you can define other functions, if you want
+        return LndS(a,0,a.length-1);
+    }
+
+    public static int LndS (int[] a, int p, int q) {
+        int m = (p + q) / 2;
+
+        if ((q - p) < 1) {
+            return 1;
+        } else {
+            int i = LndS(a,p,m);
+            int j = LndS(a,m+1,q);
+
+            if (a[m] <= a[m+1]) {
+                return i + j;
+            } else {
+                return Math.max(i,j);
+            }
+        }
     }
 
     public static void main (String[] args) {
 	// here you should declare and initialize an array, and run your 
 	// method on it to check it.
+        int[] a = new int[] {1,1,1,1,7,1,1,1,1};
+
+        System.out.println("Array:"+Arrays.toString(a));
+
+        System.out.println("Longest:"+LongestSubsequence(a));
     }
 }
