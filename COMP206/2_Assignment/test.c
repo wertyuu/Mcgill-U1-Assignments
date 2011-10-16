29851c65adddcf02c82575b629466bc66e883492
Jonathan Fokkan <jonathan.fokkan@mail.mcgill.ca>
2011-10-16 17:39:01 -0400

    MOVED files to 2_Assignment

--- COMP206/2_Assignment/test.c
+++ COMP206/2_Assignment/test.c
@@ -12,20 +12,22 @@ int main(void)
     printf("1. Input text\n2. Encrypt\n3. Decrypt\n4. Exit\nSelection: ");
 
     puts("");
-    char a = '\n';
-    char b = '\n';
-    if(a==b)puts("yen");
-    
-    while(1){
-        char line[50];
-        gets(line);
-        printf("%s", line);
-        int i;
-        for(i=0;i<50;i++) {
-            if(line[i]=='\0') {
-                break;
-            }
-        }
+    int a = 'z';
+    char b = 'z';
+
+    int in = 100;
+    //while (in > 26) {
+     //   in = in-26;
+    //}
+    a = a+in;
+
+    while (a > 'z'){
+        a = a-('z'-'a'+1);
     }
+    
+    b= b+25;
+    putchar(a);
+    puts("");
+    
     return 0;
 }
