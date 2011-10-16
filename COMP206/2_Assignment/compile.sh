29c48a03be5501cf50c265cc71e7e239ed1a3652
Jonathan Fokkan <jonathan.fokkan@mail.mcgill.ca>
2011-10-16 17:36:46 -0400

    FINISHED Q3

--- COMP206/2_Assignment/compile.sh
+++ COMP206/2_Assignment/compile.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ $1 != '-o' ];then
+if [ $# == 0 ] || [ $1 != '-o' ];then
     echo -e "\nerror: expected first argument -o\n"
     echo -e "USAGE: compile -o filename [-clean] [-backup] [-archive] [-help] cfilenames\n"
     echo -e "DESCRIPTION:\n"
@@ -9,10 +9,12 @@ if [ $1 != '-o' ];then
     echo -e "\t-backup, \toptional argument. copies all .c and .h files into backup dir.\n"
     echo -e "\t-archive, \toptional argument. TARS source dir and moves archive to backup dir.\n"
     echo -e "\t-help, \t\toptional argument. shows USAGE and DESCRIPTION.\n"
-    echo -e "\tcfilenames, \toptional argument. Source files to be compiled.\n"
+    echo -e "\tcfilenames, \tnon-optional argument. Source files to be compiled.\n"
     echo -e "EXAMPLE:\n"
     echo -e "\tcompile -o myFile -clean -backup -archive -help f1.c f2.c f3.c\n"
 else
+    #sets a hard limit to how many c files to 20
+    index=0
     for var in $*
     do
         case $var in
@@ -34,8 +36,13 @@ else
             echo -e "\tcfilenames, \toptional argument. Source files to be compiled.\n"
             echo -e "EXAMPLE:\n"
             echo -e "\tcompile -o myFile -clean -backup -archive -help f1.c f2.c f3.c\n";;
+        '*.c')
+            cfiles[$index]=$var
+            index=`expr $index + 1`;;
         esac
     done
+
+    gcc -o $2 ${cfile[*]}
 
         #if [ $var = '-clean' ];then
         #    rm *.o
