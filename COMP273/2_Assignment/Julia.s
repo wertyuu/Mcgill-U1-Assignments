d77b77980ca66c900fc58b74bdddb3114d4fafca
Jonathan Fokkan <jonathan.fokkan@mail.mcgill.ca>
2011-10-31 18:08:40 -0400

    FINISHED Q1

---
+++ COMP273/2_Assignment/Julia.s
@@ -0,0 +1,149 @@
+    .globl main
+main:
+    # input
+    ## value of a
+    li $v0, 4 
+    la $a0, aPrompt
+    syscall
+    li $v0, 6
+    syscall
+    mov.s $f4, $f0          #store a in $f4
+
+    ## value of b
+    li $v0, 4 
+    la $a0, bPrompt
+    syscall
+    li $v0, 6
+    syscall
+    mov.s $f6, $f0           #store b in $f6
+
+    # set outerloop counter as $t0
+    li $t0, 0
+    
+
+    # load y into $f10
+    l.s $f10, y
+
+    # load x_inc into $f16
+    l.s $f16, x_inc
+    
+    #load y_dec into $f18
+    l.s $f18, y_dec
+
+outerloop:
+    beq $t0, 40, outerloopdone
+
+    # load x into $f8
+    l.s $f8, x
+
+    # set innerloop counter as $t1
+    li $t1, 0
+
+innerloop:
+    beq $t1, 80, innerloopdone
+
+    # set julia iterations counter, n,
+    # as $t2
+    li $t2, 0
+
+    # copy over x onto new temporary registers
+    mov.s $f26, $f8
+
+    # copy over y onto new temporary registers
+    mov.s $f28, $f10
+
+juliaiterations:
+    
+    beq $t2, 1000, bounded
+        
+    add $sp, $sp, -8
+    swc1 $f26, 0($sp)
+    swc1 $f28, 4($sp)
+    
+    mul.s $f26, $f26, $f26          # temp x^2
+    mul.s $f28, $f28, $f28          # temp y^2
+    add.s $f26, $f26, $f28
+
+    l.s $f28, million
+    c.lt.s $f28, $f26
+    lwc1 $f26, 0($sp)
+    lwc1 $f28, 4($sp)
+    add $sp, $sp, 8
+    bc1t unbounded
+
+    # x^2 
+    mul.s $f20, $f26, $f26
+    # y^2
+    mul.s $f22, $f28, $f28
+    # x^2-y^2 in $f20
+    sub.s $f20, $f20, $f22
+
+    # 2xy
+    mul.s $f22, $f26, $f28
+    l.s $f24, two
+    mul.s $f22, $f22, $f24 
+
+    # new x
+    add.s $f26, $f20, $f4
+
+    # new y
+    add.s $f28, $f22, $f6
+
+    # increment n
+    add $t2, $t2, 1
+
+    j juliaiterations
+
+bounded:
+    li $v0, 4
+    la $a0, space
+    syscall
+    
+    # increment x
+    add.s $f8, $f8, $f16
+    # increment innerloop counter
+    add $t1, $t1, 1
+
+    j innerloop
+
+unbounded:
+    li $v0, 1
+    li $t3, 10
+    div $t2, $t3
+    mfhi $a0
+    syscall
+
+    # increment x
+    add.s $f8, $f8, $f16
+    # increment innerloop counter
+    add $t1, $t1, 1
+    j innerloop
+
+innerloopdone:
+    # increment outer loop counter
+    add $t0, $t0, 1
+
+    # decrement y
+    sub.s $f10, $f10, $f18
+
+    # print newline
+    li $v0, 4
+    la $a0, newline
+    syscall
+    j outerloop
+
+outerloopdone:
+    li $v0, 10
+    syscall
+
+    .data
+aPrompt: .asciiz "Enter the value of a: "
+bPrompt: .asciiz "Enter the value of b: "
+newline: .asciiz "\n"
+space: .asciiz " "
+million: .float 1000000
+two: .float 2
+x: .float -2.0
+x_inc: .float 0.05
+y: .float 2.0
+y_dec: .float 0.1
---
+++ COMP273/2_Assignment/JuliaIterate.s
@@ -0,0 +1,155 @@
+    .globl  main
+main:
+    # input section
+    ## value of a
+    li $v0, 4 
+    la $a0, aPrompt
+    syscall
+    li $v0, 6
+    syscall
+    mov.s $f6, $f0          #store a in $f6
+
+    ## value of b
+    li $v0, 4 
+    la $a0, bPrompt
+    syscall
+    li $v0, 6
+    syscall
+    mov.s $f8, $f0           #store b in $f8
+
+    ## value of x0
+    li $v0, 4 
+    la $a0, xPrompt
+    syscall
+
+    li $v0, 6
+    syscall
+    mov.s $f10, $f0         #store x0 in $f10
+
+    ## value of y0
+    li $v0, 4
+    la $a0, yPrompt
+    syscall
+    li $v0, 6
+    syscall
+    mov.s $f16, $f0         #store y0 in $f16
+
+    ## number of iterations
+    li $v0, 4
+    la $a0, nPrompt
+    syscall
+    li $v0, 5
+    syscall
+    move $t0, $v0            #store n in $t0
+    
+    li $v0, 4
+    la $a0, lbasecase       #print (x0, y0) = (
+    syscall
+    li $v0, 2
+    mov.s $f12, $f10        #print x0
+    syscall
+    li $v0, 4
+    la $a0, comma           #print ,
+    syscall
+    
+    li $v0, 2               #print y0
+    mov.s $f12, $f16
+    syscall
+
+    li $v0, 4               #print )
+    la $a0, cBraq
+    syscall
+    la $a0, newline         # print newline
+    syscall
+
+    # Counter is in $t1
+    li $t1, 1
+    add $t0, $t0, 1
+
+loop:
+    beq $t1, $t0, Done
+
+    # f(x,y) = ( x^2-y^2+a , 2xy+b )
+    # compute f(x,y) and new x is stored in $f10 and
+    # new y is stored in $f16
+    
+    # x^2 
+    mul.s $f20, $f10, $f10
+    # y^2
+    mul.s $f22, $f16, $f16
+    # x^2-y^2 in $f20
+    sub.s $f20, $f20, $f22
+
+    # 2xy
+    mul.s $f22, $f10, $f16
+    l.s $f24, two
+    mul.s $f22, $f22, $f24 
+
+    # new x
+    add.s $f10, $f20, $f6
+
+    # new y
+    add.s $f16, $f22, $f8
+
+    li $v0, 4               # print (x
+    la $a0, rBraq
+    syscall
+    li $v0, 1               # print counter
+    move $a0, $t1
+    syscall
+    li $v0, 4               # print ,
+    la $a0, comma
+    syscall
+    li $v0, 4               # print y
+    la $a0, y
+    syscall
+    li $v0, 1               # print counter
+    move $a0, $t1
+    syscall
+    li $v0, 4               # print ) = (
+    la $a0, lBraq
+    syscall
+    li $v0, 2               # print xi
+    mov.s $f12, $f10
+    syscall
+    li $v0, 4               # print comma
+    la $a0, comma
+    syscall
+    li $v0, 2               # print yi
+    mov.s $f12, $f16
+    syscall
+    li $v0, 4               # print )
+    la $a0, cBraq
+    syscall
+    la $a0, newline         # print newline
+    syscall
+
+
+
+    addi $t1, $t1, 1         #increase counter by 1
+
+    j loop
+
+    
+
+
+Done:
+    li $v0, 10
+    syscall
+    
+
+    .data
+aPrompt: .asciiz "Enter the value of a: "
+bPrompt: .asciiz "Enter the value of b: "
+xPrompt: .asciiz "Enter the value of x0: "
+yPrompt: .asciiz "Enter the value of y0: "
+nPrompt: .asciiz "Enter the number of iterations n: "
+newline: .asciiz "\n"
+two: .float 2
+lbasecase: .asciiz "(x0, y0) = ("
+rBraq: .asciiz "(x"
+comma: .asciiz ", "
+y: .asciiz "y"
+lBraq: .asciiz ") = ("
+cBraq: .asciiz ")"
+
