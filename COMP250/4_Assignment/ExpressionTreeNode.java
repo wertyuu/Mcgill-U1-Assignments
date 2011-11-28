e47a2f6d0754240e0eba9976e9688377d0514fce
Jonathan Fokkan <jonathan.fokkan@mail.mcgill.ca>
2011-11-28 16:58:42 -0500

    Finished Q4

--- COMP250/4_Assignment/ExpressionTreeNode.java
+++ COMP250/4_Assignment/ExpressionTreeNode.java
@@ -166,19 +166,102 @@ public class ExpressionTreeNode {
 public double evaluate(double x) throws ExpressionTreeNodeException {
 
  // Write your code here
+
+    if ( type == ExpressionTreeNode.VALUE ) {
+        if ( value.charAt(0) == 'x' ) return x;
+        else return (double)Integer.parseInt(value);
+    } else if ( type == ExpressionTreeNode.FUNCTION ) {
+        if ( value.charAt(0) == 'c' ) {
+            return Math.cos( getLeftChild().evaluate(x) );
+        } else if ( value.charAt(0) == 's' ) {
+            return Math.sin( getLeftChild().evaluate(x) );
+        } else if ( value.charAt(0) == 'e' ) {
+            // only possibility left is exp function
+            return Math.exp( getLeftChild().evaluate(x) );
+        } else {
+            throw new ExpressionTreeNodeException("Invalid Function; should be either cos, sin or exp");
+        }
+    } else {
+        // only possibility left is an operator
+        if ( value.charAt(0) == '+' ) {
+            return ( getLeftChild().evaluate(x) + getRightChild().evaluate(x) );
+        } else if ( value.charAt(0) == '-' ) {
+            return ( getLeftChild().evaluate(x) - getRightChild().evaluate(x) );
+        } else if ( value.charAt(0) == '*' ) {
+            return ( getLeftChild().evaluate(x) * getRightChild().evaluate(x) );
+        } else if ( value.charAt(0) == '/' ) {
+            // only possibility left is '/' operator
+            return ( getLeftChild().evaluate(x) / getRightChild().evaluate(x) );
+        } else {
+            throw new ExpressionTreeNodeException("Invalid operator; should be either +, -, * or /");
+        }
+    }
 }
 
 
     /* Returns the root of a new expression tree representing the derivative of the
     expression represented by the tree rooted at the node on which it is called ***/
-    public ExpressionTreeNode differentiate() {
-		// WRITE YOUR CODE HERE
-
-		return null; // remove this
+    public ExpressionTreeNode differentiate() throws ExpressionTreeNodeException {
+        if ( type == ExpressionTreeNode.VALUE ) {
+            if ( value.charAt(0) == 'x' ) {
+                ExpressionTreeNode result = new ExpressionTreeNode();
+                result.setValue("1");
+                result.setType(0);
+                return result;
+            } else {
+                ExpressionTreeNode result = new ExpressionTreeNode();
+                result.setValue("0");
+                result.setType(0);
+                return result;
+            }
+        } else if ( type == ExpressionTreeNode.FUNCTION ) {
+            if ( value.charAt(0) == 's' ) {
+                // d/dx sin = cos
+                ExpressionTreeNode result = deepCopy();
+                result.setValue("cos");
+                result.setLeftChild( getLeftChild().differentiate() );
+                return result;
+            } else if ( value.charAt(0) == 'c' ) {
+                // d/dx cos = -sin
+                ExpressionTreeNode result = new ExpressionTreeNode();
+                ExpressionTreeNode child = new ExpressionTreeNode();
+                ExpressionTreeNode sin = new ExpressionTreeNode();
+                result.setValue("-");
+                result.setType(2);
+                child.setValue("0");
+                child.setType(0);
+                result.setLeftChild(child);
+                sin.setValue("sin");
+                sin.setType(1);
+                sin.setLeftChild( getLeftChild().differentiate() );
+                result.setRightChild(sin);
+                return result;
+            } else {
+                // Only possibility is exp
+                ExpressionTreeNode result = deepCopy();
+                return result;
+            }
+        } else { // if ( type == ExpressionTreeNode.OPERATOR ) {
+            // Only possibility is an Operator
+            ExpressionTreeNode result = new ExpressionTreeNode();
+            result.setValue( getValue() );
+            result.setType(2);
+            result.setLeftChild( getLeftChild().differentiate() );
+            result.setRightChild( getRightChild().differentiate() );
+            return result;
+        }
     }
  
-	public ExpressionTreeNode add (ExpressionTreeNode t) {
+	public ExpressionTreeNode add (ExpressionTreeNode t) throws ExpressionTreeNodeException {
 	// Write your code here	
+            ExpressionTreeNode child1 = t.deepCopy();
+            ExpressionTreeNode child2 = deepCopy();
+            ExpressionTreeNode result = new ExpressionTreeNode();
+            result.setType(2);
+            result.setValue("+");
+            result.setLeftChild(child1);
+            result.setRightChild(child2);
+            return result;
 	}
  
 	
@@ -186,11 +269,23 @@ public double evaluate(double x) throws ExpressionTreeNodeException {
 	public static void main(String[] args) throws ExpressionTreeNodeException {
 		String s;
 		s ="((x)+(4))*(cos(x))";		
+                String sss = "((2)-(sin(x)))*(exp(x)+(6))"; 
 		ExpressionTreeNode n = new ExpressionTreeNode(s);		
 		System.out.println(n.toString());
-		ExpressionTreeNode ss = new ExpressionTreeNode("(x)");
-		System.out.println(ss.evaluate(0));
+		ExpressionTreeNode nn = new ExpressionTreeNode("(x)");
+		System.out.println(n.evaluate(0));
+                
+                ExpressionTreeNode nnn = new ExpressionTreeNode(sss);
+                System.out.println(nnn.toString());
+                System.out.println(nnn.evaluate(0));
+
+                ExpressionTreeNode nnnn = n.add(nnn);
+                System.out.println(nnnn.toString());
+                System.out.println(nnnn.evaluate(0));
+
+                System.out.println(n.differentiate().toString());
 		
+                System.out.println(nn.differentiate().toString());
 	}
 
 }
---
+++ COMP250/4_Assignment/test.java
@@ -0,0 +1,7 @@
+public class test {
+    public static void main(String[] args) {
+        String aString = "Hello World";
+        char a = aString.charAt(1);
+        System.out.println(a);
+    }
+}
